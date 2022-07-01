d305d699807bacad18453d53f1b1afb5b1226b59
Ashen Etugala <66189779+Ashenboy@users.noreply.github.com>
2022-07-01 07:28:26 +0530

    Fix abuse steal fuel

--- mt-stealfuel/client.lua
+++ mt-stealfuel/client.lua
@@ -6,7 +6,6 @@ RegisterNetEvent('mt-stealfuel:client:RoubarCombustivel', function()
     QBCore.Functions.TriggerCallback('QBCore:HasItem', function(HasItem)
 
         if HasItem then
-
             SetVehicleDoorOpen(veiculo, 4, false, false)
             QBCore.Functions.Progressbar('roubar_combustivel', 'STEALING VEHICLE FUEL...', 7500, false, true, {
                 disableMovement = true,
@@ -18,11 +17,16 @@ RegisterNetEvent('mt-stealfuel:client:RoubarCombustivel', function()
                 anim = 'fixing_a_ped',
                 flags = 16,
             }, {}, {}, function()
+                local fuelLevel = exports["LegacyFuel"]:GetFuel(veiculo)
                 ClearPedTasks(PlayerPedId())
-                TriggerServerEvent('mt-stealfuel:server:DarJerryCan')
-                QBCore.Functions.Notify('You have robbed vehicle fuel!', 'success', 7500)
-                exports['LegacyFuel']:SetFuel(veiculo, 0)
                 SetVehicleDoorShut(veiculo, 4, false, false)
+                if fuelLevel > 0 then
+                    TriggerServerEvent('mt-stealfuel:server:DarJerryCan')
+                    QBCore.Functions.Notify('You have robbed vehicle fuel!', 'success', 7500)
+                    exports['LegacyFuel']:SetFuel(veiculo, 0)
+                else
+                    QBCore.Functions.Notify('This vehicle does not have enough fuel!', 'error', 7500)
+                end
             end)
         else
             QBCore.Functions.Notify('You dont have a hose!', 'error', 7500)
